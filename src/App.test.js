6b4b207e9e89a5302a93355efe8d77a5a2c6a298
Leducdat <leducdat01@gmail.com>
2024-10-03 20:42:55 +0700

    Update App.test.js
    test lần 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 //test push code
+//test lần 2
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
